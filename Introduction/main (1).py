744692fe89a5ad6469421a21543a0cc0a59844e2
Rylelinnn <144930839+Rylelinnn@users.noreply.github.com>
2023-09-13 19:18:36 +0400

    Add files via upload

---
+++ Introduction/main (1).py
@@ -0,0 +1,12 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+print ('Hi my name is Cyle or you can call me Cyla ')
+print ('I love to bake and binge on series o⁠^⁠ ▽ ⁠^⁠o ')
+print ('thats all folks thank you ⁠⁠✿ ')
